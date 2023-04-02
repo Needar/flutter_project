10cc01e6630eca2a82a340354e45f09aba1bafb2
needar <nidar.zher@gmail.com>
2023-04-02 04:45:20 -0700

    l

--- food/lib/sdra arif/tab1.dart
+++ food/lib/sdra arif/tab1.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'class.dart';
+
 class tab1 extends StatelessWidget {
   const tab1({super.key});
 
@@ -9,18 +10,19 @@ class tab1 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView.builder(
-      itemCount: TripList.length,
-      itemBuilder: (context, index) {
-      List<User> user = TripList;
-      return  Card(
-        child: ListTile(
-          leading:Image.asset(TripList[index].emg),
-          title: Text(TripList[index].name),
-          subtitle: Text(TripList[index].day),
-          trailing: Icon(Icons.menu),
-        ),
-      );
-     },
-   ),
- );
-}}
+        itemCount: TripList.length,
+        itemBuilder: (context, index) {
+          List<User> user = TripList;
+          return Card(
+            child: ListTile(
+              leading: Image.asset(TripList[index].emg),
+              title: Text(TripList[index].name),
+              subtitle: Text(TripList[index].day),
+              trailing: Icon(Icons.menu),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
